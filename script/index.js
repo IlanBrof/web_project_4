0ee1dc4323ec57d87e5c868bc375d2bbb9b33290
IlanBrof <ilanbrof1@gmail.com>
2021-10-20 17:32:04 +0300

    1st_iteration_complete

---
+++ script/index.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector('.popup-menu');
+let profileMenuEditForm = popup.querySelector('.popup-menu__edit-form');
+let menuCloseButton = popup.querySelector('.popup-menu__close-button');
+let menuInputName = popup.querySelector('.popup-menu__input_name');
+let menuInputTitle = popup.querySelector('.popup-menu__input_title');
+let profileName = document.querySelector('.profile-menu__full-name');
+let profileTitleDescription = document.querySelector('.profile-menu__title');
+let profileEditButton = document.querySelector('.profile-menu__edit-button');
+
+function popupMenuOpen() {
+  popup.classList.add('popup-menu_opened');
+  menuInputName.value = profileName.textContent;
+  menuInputTitle.value = profileTitleDescription.textContent;
+}
+
+function popupCloseMenu() {
+  popup.classList.remove('popup-menu_opened');
+}
+
+profileEditButton.addEventListener('click', popupMenuOpen);
+menuCloseButton.addEventListener('click', popupCloseMenu);
+
+profileMenuEditForm.addEventListener('submit', (event) => {
+  profileName.textContent = menuInputName.value;
+  profileTitleDescription.textContent = menuInputTitle.value;
+  event.preventDefault();
+  popupCloseMenu();
+});
